46b84b377d1873193cd7fc4f96bcac76d90d7655
Jonathan Herrera <jokherre@ucsc.edu>
2026-05-12 17:12:44 -0700

    changed mapping, hopefully improved block placement/deletion

--- asgn3/src/World.js
+++ asgn3/src/World.js
@@ -239,47 +239,9 @@ let g_bodySway = 0;
 let g_pokeAnimation = false;
 let g_pokeStartTime = 0;
 
-
 // Set up actions for the HTML UI elements
 function addActionsforHtmlUI() {
 
-  // Button Events
-  // document.getElementById('animationLeftWingOffButton').onclick = function() {g_leftWingAnimation = false;}
-  // document.getElementById('animationLeftWingOnButton').onclick = function() {g_leftWingAnimation = true;}
-
-  // document.getElementById('animationRightWingOffButton').onclick = function() {g_rightWingAnimation = false;}
-  // document.getElementById('animationRightWingOnButton').onclick = function() {g_rightWingAnimation = true;}
-
-  // document.getElementById('animationLeftLegOffButton').onclick = function() {g_leftLegAnimation = false;}
-  // document.getElementById('animationLeftLegOnButton').onclick = function() {g_leftLegAnimation = true;}
-
-  // document.getElementById('animationRightLegOffButton').onclick = function() {g_rightLegAnimation = false;}
-  // document.getElementById('animationRightLegOnButton').onclick = function() {g_rightLegAnimation = true;}
-
-  // // Joint Slider Events
-  //   // Left Wing
-  // document.getElementById('leftWingJoint1').addEventListener('mousemove', function() {g_leftWingAngle1 = this.value; renderAllShapes(); });
-  // document.getElementById('leftWingJoint2').addEventListener('mousemove', function() {g_leftWingAngle2 = this.value; renderAllShapes(); });
-  // document.getElementById('leftWingJoint3').addEventListener('mousemove', function() {g_leftWingAngle3 = this.value; renderAllShapes(); });
-  //   // Right Wing
-  // document.getElementById('rightWingJoint1').addEventListener('mousemove', function() {g_rightWingAngle1 = this.value; renderAllShapes(); });
-  // document.getElementById('rightWingJoint2').addEventListener('mousemove', function() {g_rightWingAngle2 = this.value; renderAllShapes(); });
-  // document.getElementById('rightWingJoint3').addEventListener('mousemove', function() {g_rightWingAngle3 = this.value; renderAllShapes(); });
-
-  //   // Left Leg
-  // document.getElementById('leftLegJoint1').addEventListener('mousemove', function() {g_leftLegAngle1 = this.value; renderAllShapes(); });
-  // document.getElementById('leftLegJoint2').addEventListener('mousemove', function() {g_leftLegAngle2 = this.value; renderAllShapes(); });
-  // document.getElementById('leftLegJoint3').addEventListener('mousemove', function() {g_leftLegAngle3 = this.value; renderAllShapes(); });
-
-  //   // Right Leg
-  // document.getElementById('rightLegJoint1').addEventListener('mousemove', function() {g_rightLegAngle1 = this.value; renderAllShapes(); });
-  // document.getElementById('rightLegJoint2').addEventListener('mousemove', function() {g_rightLegAngle2 = this.value; renderAllShapes(); });
-  // document.getElementById('rightLegJoint3').addEventListener('mousemove', function() {g_rightLegAngle3 = this.value; renderAllShapes(); });
-
-  // // Angle Slider Events
-  // document.getElementById('angleSlideX').addEventListener('mousemove', function() { g_globalAngleX = this.value; renderAllShapes(); });
-  // document.getElementById('angleSlideY').addEventListener('mousemove', function() { g_globalAngleY = this.value; renderAllShapes(); });
-  
 }
 
 function initTextures() {
@@ -339,23 +301,19 @@ function main() {
   });
 
   // Left Clicking, Right Clicking
-  // canvas.onmousedown = function(ev) {
-  //   let block = getBlockInFront();
-    
-  //   if (!inBounds(block.x, block.z)) {
-  //     return;
-  //   }
+  canvas.onmousedown = function(ev) {
+    // Left click to delete
+    if (ev.button === 0) {
+      // g_map[block.x][block.z] = 0;
+      breakBlock();
+    }
 
-  //   // Left click to delete
-  //   if (ev.button === 0) {
-  //     g_map[block.x][block.z] = 0;
-  //   }
-
-  //   // Right click to place
-  //   if (ev.button === 2) {
-  //     g_map[block.x][block.z] = 1;
-  //   }
-  // };
+    // Right click to place
+    if (ev.button === 2) {
+      // g_map[block.x][block.z] = 1;
+      placeBlock();
+    }
+  };
 
   canvas.oncontextmenu = (ev) => ev.preventDefault();  
 
@@ -567,18 +525,22 @@ function renderScene() {
   var wall = new Cube();
   wall.color = [0.75, 0.75, 0.75, 1];
   wall.textureNum = 3;
-  wall.matrix.translate(-16, -1, -16);
+  // wall.matrix.translate(-16, -1, -16);
+
   for (let i = 0; i < 3; i++) {   
     for (let x = 0; x < 32; x++) {
-      for (let y = 0; y < 32; y++) {
-        if (x == 0 || y == 0 || x == 31 || y == 31) {
+      for (let z = 0; z < 32; z++) {
+        if (x == 0 || z == 0 || x == 31 || z == 31) {
+          wall.matrix.setIdentity()
+          wall.matrix.translate(x - 16, i-1, z - 16);
           wall.renderFast();
+
         }
-        wall.matrix.translate(0,0,1);
+        // wall.matrix.translate(0,0,1);
       }
-      wall.matrix.translate(1,0,-32);
+      // wall.matrix.translate(1,0,-32);
     }
-    wall.matrix.translate(-32,1,0);
+    // wall.matrix.translate(-32,1,0);
   }
 
   // Draw Maze
@@ -590,85 +552,83 @@ function renderScene() {
   sendTextToHTML("ms: " + Math.floor(duration) + " fps: " + Math.floor(1000/duration), "performance");
 }
 
-function inBounds(x, z) {
-  return (x >= 0) && (x < g_map.length) && (z >= 0) && (z < g_map[0].length);
+function getBlockInFront() {
+  let eye = g_camera.eye.elements;
+  let at = g_camera.at.elements;
+
+  let dx = at[0] - eye[0];
+  let dy = at[1] - eye[1];
+  let dz = at[2] - eye[2];
+
+  let len = Math.sqrt(dx * dx + dy * dy + dz * dz);
+  dx /= len;
+  dy /= len;
+  dz /= len;
+  
+  for (let t = 0; t < 6; t += 0.1) {
+    let worldX = Math.floor(eye[0] + dx * t + g_maze.length / 2);
+    let worldY = Math.floor((eye[1] + dy * t) / 0.5);
+    let worldZ = Math.floor(eye[2] + dz * t + g_maze.length / 2);
+    
+    if (worldX >= 0 && worldZ >= 0 && worldX < g_maze.length && worldZ < g_maze.length) {
+      if (worldY < g_maze[worldX][worldZ]) {
+        return { x: worldX, y: worldY, z: worldZ };
+      }
+    }
+  }
+  
+  return null;
 }
 
-function getBlockInFront() {
-  let dir = new Vector3([
-    g_camera.at.elements[0] - g_camera.eye.elements[0],
-    0,
-    g_camera.at.elements[2] - g_camera.eye.elements[2]
-  ]);
+function placeBlock() {
+  let block = getBlockInFront();
 
-  dir.normalize();
+  if (!block) {
+    return;
+  }
 
-  let dist = 1.5;
+  g_maze[block.x][block.z]++;
+}
 
-  let tx = g_camera.eye.elements[0] + dir.elements[0] * dist;
-  let tz = g_camera.eye.elements[2] + dir.elements[2] * dist;
+function breakBlock() {
+  let block = getBlockInFront();
 
-  let x = Math.floor(tx + MAP_OFFSET);
-  let z = Math.floor(tz + MAP_OFFSET);
+  if (!block) {
+    return;
+  }
 
-  return { x, z };
+  if (g_maze[block.x][block.z] > 0) {
+    g_maze[block.x][block.z]--;
+  }
 }
 
 function drawMaze() {
   let cube = new Cube();
   cube.textureNum = 4;
   cube.color = [0.4, 0.3, 0.2, 1];
-  cube.matrix.translate(-8, -1, -8);
-  cube.matrix.scale(1,0.5,1);
 
+  // cube.matrix.translate(-8, -1, -8);
+  
   for (let x = 0; x < g_maze.length; x++) {
-    for (let y = 0; y < g_maze[0].length; y++) {
-      if (g_maze[x][y] == 1) {
+    for (let z = 0; z < g_maze[0].length; z++) {
+      let height = g_maze[x][z];
+      
+      for (let y = 0; y < g_maze[x][z]; y++) {
+        cube.matrix.setIdentity();
+        cube.matrix.translate ( x - g_maze.length / 2, y - 1, z - g_maze[x].length / 2 );
+        cube.matrix.scale(1,0.5,1);
+
         cube.renderFast();
+        // cube.matrix.translate(0,1,0);
       }
-      cube.matrix.translate(0,0,1);
+      // cube.matrix.translate(0,-g_maze[x][z],1);
     } 
-    cube.matrix.translate(1,0,-16);
+    // cube.matrix.translate(1,0,-16);
   }
 }
 
 // Draws shapes for penguin
 function renderAllShapes() {
-//   // Check the time at the start of this function
-//   var startTime = performance.now();
-
-//   // Pass the projection matrix
-//   var projMat = new Matrix4();
-//   projMat.setPerspective(50, 1*canvas.width/canvas.height, .1, 100);
-//   gl.uniformMatrix4fv(u_ProjectionMatrix, false, projMat.elements);
-
-//   // Pass the view matrix
-//   var viewMat = new Matrix4();
-// //   viewMat.setLookAt(0,0,4, 0,0,-100, 0,1,0); // (eye, at, up)
-// //   viewMat.setLookAt(g_eye[0], g_eye[1], g_eye[2], g_at[0], g_at[1], g_at[2], g_up[0], g_up[1], g_up[2]); // (eye, at, up)
-  
-//     // viewMat.setLookAt(
-//         // g_camera.eye.x, g_camera.eye.y, g_camera.eye.z,
-//         // g_camera.at.x, g_camera.at.y, g_camera.at.z,
-//         // g_camera.up.x, g_camera.up.y, g_camera.up.z);
-    
-//     viewMat.setLookAt(
-//         g_camera.eye.elements[0], g_camera.eye.elements[1], g_camera.eye.elements[2],
-//         g_camera.at.elements[0] ,  g_camera.at.elements[1],  g_camera.at.elements[2],
-//         g_camera.up.elements[0] ,  g_camera.up.elements[1],  g_camera.up.elements[2]);
-
-//     gl.uniformMatrix4fv(u_ViewMatrix, false, viewMat.elements);
-
-//   // Pass the matrix to a u_ModelMatrix attribute
-//   var globalRotMat = new Matrix4();
-//   globalRotMat.rotate(g_globalAngleX, 0,1,0);
-//   globalRotMat.rotate(g_globalAngleY, 1,0,0);
-//   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
-
-//   // Clear <canvas>
-//   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-//   gl.clear(gl.COLOR_BUFFER_BIT);w
-
   // Color variables:
   let white = [0.9,0.9,0.9,1];
   let black = [0.1,0.1,0.1,1];
@@ -907,233 +867,3 @@ function sendTextToHTML(text, htmlID) {
   }
   htmlElm.innerHTML = text;
 }
-
-
-
-// // Color variables:
-//   let white = [0.9,0.9,0.9,1];
-//   let black = [0.1,0.1,0.1,1];
-
-//   // Body
-//   let lowerBody = new Cube();
-//   lowerBody.color = black;
-//   lowerBody.textureNum = -2;   
-//   lowerBody.matrix.setTranslate(-0.25, -0.5, 0);
-//   if (g_pokeAnimation) {
-//     lowerBody.matrix.translate(0.275, 0.35, 0.2); 
-//     lowerBody.matrix.rotate(g_bodySway, 0,0,1);
-//     lowerBody.matrix.translate(-0.275, -0.35, -0.2);
-//   }
-//   let bodyLocationMat = new Matrix4(lowerBody.matrix);
-//   lowerBody.matrix.translate(0.025,0,0.025);
-//   lowerBody.matrix.scale(0.55,0.7,0.4);
-
-//   let lowerBodyWhite = new Cube();
-//   lowerBodyWhite.matrix = new Matrix4(lowerBody.matrix);
-//   lowerBodyWhite.color = white;
-//   lowerBodyWhite.matrix.scale(0.8,1.01,0.8);
-//   lowerBodyWhite.matrix.translate(0.1,-0.001,-0.001);
-//   lowerBodyWhite.render();
-//   lowerBody.render();
-  
-//   let midBody = new Cube();
-//   midBody.color = black;
-//   midBody.matrix = new Matrix4(bodyLocationMat);
-//   midBody.matrix.translate(0.05,0.7,0.05);
-//   let midBodyLocationMat = new Matrix4(midBody.matrix);
-//   midBody.matrix.scale(0.5,0.15,0.35);
-//   midBody.render();
-
-//   let midBodyWhite = new Cube();
-//   midBodyWhite.matrix = new Matrix4(midBody.matrix);
-//   midBodyWhite.color = white;
-//   midBodyWhite.matrix.scale(0.8,1.01,0.8);
-//   midBodyWhite.matrix.translate(0.1,-0.001,-0.001);
-//   midBodyWhite.render();
-//   midBody.render();
-
-//   let upperBody = new Cube();
-//   upperBody.matrix = new Matrix4(midBodyLocationMat);
-//   upperBody.color = black;
-//   upperBody.matrix.translate(0.05, 0.15, 0.05);
-//   let upperBodyLocationMat = new Matrix4(upperBody.matrix);
-//   upperBody.matrix.scale(0.4,0.1,0.275)
-//   upperBody.render();
-
-//   let upperBodyWhite = new Cube();
-//   upperBodyWhite.matrix = new Matrix4(upperBody.matrix);
-//   upperBodyWhite.color = white;
-//   upperBodyWhite.matrix.scale(0.8,1.01,0.8);
-//   upperBodyWhite.matrix.translate(0.1,-0.001,-0.001);
-//   upperBodyWhite.render();
-//   upperBody.render();
-
-//   // Head
-//   let head = new Cube();
-//   head.color = black;
-//   head.matrix = new Matrix4(upperBodyLocationMat);
-//   head.matrix.translate(0.025, 0.1, 0.025);
-//   let headLocationMat = new Matrix4(head.matrix);
-//   head.matrix.scale(0.35,0.3,0.25);
-//   head.render();
-  
-//   let upperBeak = new Cube();
-//   upperBeak.color = [1,0.6,0,1];
-//   upperBeak.matrix = new Matrix4(headLocationMat);
-//   upperBeak.matrix.translate(0.125, 0.1, -0.08);
-//   upperBeak.matrix.scale(0.1,0.08,0.1);
-//   upperBeak.matrix.rotate(45,45,0,1);
-//   upperBeak.render();
-
-//   let outerEyeRight = new Cube();
-//   outerEyeRight.color = [0,0,0,0];
-//   outerEyeRight.matrix = new Matrix4(headLocationMat);
-//   outerEyeRight.matrix.translate(0.025, 0.1, -0.01);
-//   let outerEyeRightLocationMat = new Matrix4(outerEyeRight.matrix);
-//   outerEyeRight.matrix.scale(0.08,0.1,0.025);
-//   outerEyeRight.render();
-
-//   let innerEyeRight = new Cube();
-//   innerEyeRight.color = [0,0,1,1];
-//   innerEyeRight.matrix = new Matrix4(outerEyeRightLocationMat);
-//   innerEyeRight.matrix.translate(0.015, 0, -0.01);
-//   innerEyeRight.matrix.scale(0.06,0.08,0.1);
-//   innerEyeRight.render();
-
-//   let outerEyeLeft = new Cube();
-//   outerEyeLeft.color = [0,0,0,0];
-//   outerEyeLeft.matrix = new Matrix4(headLocationMat);
-//   outerEyeLeft.matrix.translate(0.25, 0.1, -0.01);
-//   let outerEyeLeftLocationMat = new Matrix4(outerEyeLeft.matrix);
-//   outerEyeLeft.matrix.scale(0.08,0.1,0.025);
-//   outerEyeLeft.render();
-
-//   let innerEyeLeft = new Cube();
-//   innerEyeLeft.color = [0,0,1,1];
-//   innerEyeLeft.matrix = new Matrix4(outerEyeLeftLocationMat);
-//   innerEyeLeft.matrix.translate(0.015, 0, -0.01);
-//   innerEyeLeft.matrix.scale(0.06,0.08,0.1);
-//   innerEyeLeft.render();
-
-//   // left Wing
-//   let leftWing = new Cube();
-//   leftWing.color = black;
-//   leftWing.matrix = new Matrix4(midBodyLocationMat);
-//   leftWing.matrix.translate(0.1, 0.12, 0.1);
-//   leftWing.matrix.rotate(g_leftWingAngle1, 0,0,1);
-
-//   let wingMatrixLeft = new Matrix4(leftWing.matrix);
-//   leftWing.matrix.scale(0.05,0.3,0.2);
-//   leftWing.render();
-  
-//   let leftWing2 = new Cube();
-//   leftWing2.color = black;
-//   leftWing2.matrix = new Matrix4(wingMatrixLeft);
-//   leftWing2.matrix.translate(0,0.3,0);
-//   leftWing2.matrix.rotate(g_leftWingAngle2, 0,0,1);
-
-//   let wingMatrixLeft2 = new Matrix4(leftWing2.matrix);
-//   leftWing2.matrix.scale(0.05,0.3,0.2);
-//   leftWing2.render();
-
-//   let leftWingTip = new Pyramid();
-//   leftWingTip.color = black;
-//   leftWingTip.matrix = new Matrix4(wingMatrixLeft2);
-//   leftWingTip.matrix.translate(0,0.3,0);
-//   leftWingTip.matrix.scale(0.05,0.2,0.2);
-//   leftWingTip.matrix.rotate(g_leftWingAngle3, 0,0,1);
-//   leftWingTip.render();
-
-//   // right Wing
-//   let rightWing = new Cube();
-//   rightWing.color = black;
-//   rightWing.matrix = new Matrix4(midBodyLocationMat);
-//   rightWing.matrix.translate(0.45, 0.14, 0.1);
-//   rightWing.matrix.rotate(g_rightWingAngle1, 0,0,1);
-
-//   let wingMatrixRight = new Matrix4(rightWing.matrix);
-//   rightWing.matrix.scale(0.05,0.3,0.2);
-//   rightWing.render();
-
-//   let rightWing2 = new Cube();
-//   rightWing2.color = black;
-//   rightWing2.matrix = new Matrix4(wingMatrixRight);
-//   rightWing2.matrix.translate(0,0.3,0);
-//   rightWing2.matrix.rotate(g_rightWingAngle2, 0,0,1);
-
-//   let wingMatrixRight2 = new Matrix4(rightWing2.matrix);
-//   rightWing2.matrix.scale(0.05,0.3,0.2);
-//   rightWing2.render();
-
-//   let rightWingTip = new Pyramid();
-//   rightWingTip.color = black;
-//   rightWingTip.matrix = new Matrix4(wingMatrixRight2);
-//   rightWingTip.matrix.translate(0,0.3,0);
-//   rightWingTip.matrix.rotate(g_rightWingAngle3, 0,0,1);
-//   rightWingTip.matrix.scale(0.05,0.2,0.2);
-//   rightWingTip.render();
-
-//   // Right Thigh
-//   let rightThigh = new Cube();
-//   rightThigh.color = [0.9,0.9,0.9,1];
-//   rightThigh.matrix = new Matrix4(bodyLocationMat);
-//   // rightThigh.matrix.setTranslate(-0.2, -0.6 + g_rightLegOffsetY, 0.1 + g_rightLegOffsetZ);
-//   rightThigh.matrix.translate(0.1, -0.1 + g_rightLegOffsetY, 0.1 + g_rightLegOffsetZ);
-
-//   rightThigh.matrix.rotate(g_rightLegAngle1, 1,0,0);
-
-//   let rightThighMatrix = new Matrix4(rightThigh.matrix);
-//   rightThigh.matrix.scale(0.175,0.15,0.2);
-//   rightThigh.render();
-
-//   // Right Calf
-//   let rightCalf = new Cube();
-//   rightCalf.color = [1,0.6,0,1];
-//   rightCalf.matrix = new Matrix4(rightThighMatrix);
-//   rightCalf.matrix.translate(0.035, -0.05, 0.05);
-//   rightCalf.matrix.rotate(g_rightLegAngle2,1,0,0);
-
-//   let rightCalfMatrix = new Matrix4(rightCalf.matrix);
-//   rightCalf.matrix.scale(0.1,0.15,0.1);
-//   rightCalf.render();
-
-//   // Right Foot
-//   let rightFoot = new Cube();
-//   rightFoot.color = [1,0.6,0,1];
-//   rightFoot.matrix = new Matrix4(rightCalfMatrix);
-//   rightFoot.matrix.translate(-0.05, -0.03, -0.2);
-//   rightFoot.matrix.scale(0.2,0.05,0.3);
-//   rightFoot.matrix.rotate(g_rightLegAngle3, 1,0,0);
-//   rightFoot.render();
-
-//   // Left Thigh
-//   let leftThigh = new Cube();
-//   leftThigh.color = [0.9,0.9,0.9,1];
-//   leftThigh.matrix = new Matrix4(bodyLocationMat);
-//   // leftThigh.matrix.translate(0.35, -0.1, 0.1);
-//   leftThigh.matrix.translate(0.35, -0.1 + g_leftLegOffsetY, 0.1 + g_leftLegOffsetZ);
-//   leftThigh.matrix.rotate(g_leftLegAngle1, 1, 0, 0);
-
-//   let leftThighMatrix = new Matrix4(leftThigh.matrix);
-//   leftThigh.matrix.scale(0.175,0.15,0.2);
-//   leftThigh.render();
-
-//   // Left Calf
-//   let leftCalf = new Cube();
-//   leftCalf.color = [1,0.6,0,1];
-//   leftCalf.matrix = new Matrix4(leftThighMatrix);
-//   leftCalf.matrix.translate(0.035, -0.05, 0.05);
-//   leftCalf.matrix.rotate(g_leftLegAngle2,1,0,0);
-
-//   let leftCalfMatrix = new Matrix4(leftCalf.matrix);
-//   leftCalf.matrix.scale(0.1,0.15,0.1);
-//   leftCalf.render();
-
-//   // Left Foot
-//   let leftFoot = new Cube();
-//   leftFoot.color = [1,0.6,0,1];
-//   leftFoot.matrix = new Matrix4(leftCalfMatrix);
-//   leftFoot.matrix.translate(-0.05, -0.03, -0.2);
-//   leftFoot.matrix.scale(0.2,0.05,0.3);
-//   leftFoot.matrix.rotate(g_leftLegAngle3,1,0,0);
-//   leftFoot.render();
